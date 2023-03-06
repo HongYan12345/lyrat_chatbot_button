2187d16df7ed87a049bc357bf187196a8af8dd0a
Hongyan Xie <hongyan.xie@alumnos.upm.es>
2023-03-06 22:49:08 +0100

    update video and api

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/a3761/Desktop/esp-idf/esp-adf-master/examples/cloud_services/lyrat_chatbot_button/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/a3761/Desktop/esp-idf/.git")
+set(GIT_DIR "C:/Users/a3761/Desktop/esp-idf/esp-adf-master/examples/cloud_services/lyrat_chatbot_button/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
